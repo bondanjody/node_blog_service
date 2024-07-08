291afe8e36a405627b59c68e92cd2c1ab4057e0e
bondanjs <bondanjodys@gmail.com>
2024-07-08 16:19:17 +0700

    Mengubah penulisan kode di authController.ts menjadi menggunakan functional.

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -2,32 +2,34 @@ import { Request, Response } from 'express';
 import AuthService from '../services/authService';
 import logger from '../config/logger';
 
-export class AuthController {
-    async register(req: Request, res: Response) {
-        try {
-            const { username, password, email } = req.body;
-            const user = await AuthService.register(username, password, email);
-            res.status(201).json(user);
-        } catch (error) {
-            logger.error('Error registering user:', error);
-            res.status(500).send('Internal Server Error');
-        }
+const register = async (req: Request, res: Response) => {
+    try {
+        const { username, password, email } = req.body;
+        const user = await AuthService.register(username, password, email);
+        res.status(201).json(user);
+    } catch (error) {
+        logger.error('Error registering user:', error);
+        res.status(500).send('Internal Server Error');
     }
+};
 
-    async login(req: Request, res: Response) {
-        try {
-            const { username, password } = req.body;
-            const result = await AuthService.login(username, password);
-            if (result) {
-                res.json(result);
-            } else {
-                res.status(401).send('Invalid credentials');
-            }
-        } catch (error) {
-            logger.error('Error logging in:', error);
-            res.status(500).send('Internal Server Error');
+const login = async (req: Request, res: Response) => {
+    try {
+        const { username, password } = req.body;
+        const result = await AuthService.login(username, password);
+        if (result) {
+            res.json(result);
+        } else {
+            res.status(401).send('Invalid credentials');
         }
+    } catch (error) {
+        logger.error('Error logging in:', error);
+        res.status(500).send('Internal Server Error');
     }
-}
+};
 
-export default new AuthController();
+export default {
+    register,
+    login
+};
+
