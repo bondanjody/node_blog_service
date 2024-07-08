76eb67a978a2c2ea0440933925d4d7163d5580eb
bondanjs <bondanjodys@gmail.com>
2024-07-08 16:26:28 +0700

    Mengubah kode program 'postController.ts' menjadi bentuk functional.

--- src/controllers/postController.ts
+++ src/controllers/postController.ts
@@ -1,28 +1,40 @@
 import { Request, Response } from 'express';
 import PostService from '../services/postService';
 
-export class PostController {
-    async getAllPosts(req: Request, res: Response) {
+const getAllPosts = async (req: Request, res: Response) => {
+    try {
         const posts = await PostService.getAllPosts();
         res.json(posts);
+    } catch (error) {
+        res.status(500).send('Internal Server Error');
     }
+};
 
-    async getPostById(req: Request, res: Response) {
+const getPostById = async (req: Request, res: Response) => {
+    try {
         const post = await PostService.getPostById(Number(req.params.id));
         if (post) {
             res.json(post);
         } else {
             res.status(404).send('Post not found');
         }
+    } catch (error) {
+        res.status(500).send('Internal Server Error');
     }
+};
 
-    async createPost(req: Request, res: Response) {
+const createPost = async (req: Request, res: Response) => {
+    try {
         const { title, content, userId, categoryId } = req.body;
         const post = await PostService.createPost(title, content, userId, categoryId);
         res.status(201).json(post);
+    } catch (error) {
+        res.status(500).send('Internal Server Error');
     }
+};
 
-    async updatePost(req: Request, res: Response) {
+const updatePost = async (req: Request, res: Response) => {
+    try {
         const { title, content, userId, categoryId } = req.body;
         const post = await PostService.updatePost(Number(req.params.id), title, content, userId, categoryId);
         if (post) {
@@ -30,16 +42,28 @@ export class PostController {
         } else {
             res.status(404).send('Post not found');
         }
+    } catch (error) {
+        res.status(500).send('Internal Server Error');
     }
+};
 
-    async deletePost(req: Request, res: Response) {
+const deletePost = async (req: Request, res: Response) => {
+    try {
         const success = await PostService.deletePost(Number(req.params.id));
         if (success) {
             res.status(204).send();
         } else {
             res.status(404).send('Post not found');
         }
+    } catch (error) {
+        res.status(500).send('Internal Server Error');
     }
-}
+};
 
-export default new PostController();
+export default {
+    getAllPosts,
+    getPostById,
+    createPost,
+    updatePost,
+    deletePost
+};
