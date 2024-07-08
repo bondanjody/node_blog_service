ea4857d906b0de34c3ccce5233548be84cd309a6
bondanjs <bondanjodys@gmail.com>
2024-07-08 16:23:32 +0700

    Mengubah kode program 'categoryController.ts' menjadi bentuk functional.

--- src/controllers/categoryController.ts
+++ src/controllers/categoryController.ts
@@ -1,19 +1,27 @@
 import { Request, Response } from 'express';
 import CategoryService from '../services/categoryService';
 
-export class CategoryController {
-    async getAllCategories(req: Request, res: Response) {
+const getAllCategories = async (req: Request, res: Response) => {
+    try {
         const categories = await CategoryService.getAllCategories();
         res.json(categories);
+    } catch (error) {
+        res.status(500).send('Internal Server Error');
     }
+};
 
-    async createCategory(req: Request, res: Response) {
+const createCategory = async (req: Request, res: Response) => {
+    try {
         const { name } = req.body;
         const category = await CategoryService.createCategory(name);
         res.status(201).json(category);
+    } catch (error) {
+        res.status(500).send('Internal Server Error');
     }
+};
 
-    async updateCategory(req: Request, res: Response) {
+const updateCategory = async (req: Request, res: Response) => {
+    try {
         const { name } = req.body;
         const category = await CategoryService.updateCategory(Number(req.params.id), name);
         if (category) {
@@ -21,16 +29,27 @@ export class CategoryController {
         } else {
             res.status(404).send('Category not found');
         }
+    } catch (error) {
+        res.status(500).send('Internal Server Error');
     }
+};
 
-    async deleteCategory(req: Request, res: Response) {
+const deleteCategory = async (req: Request, res: Response) => {
+    try {
         const success = await CategoryService.deleteCategory(Number(req.params.id));
         if (success) {
             res.status(204).send();
         } else {
             res.status(404).send('Category not found');
         }
+    } catch (error) {
+        res.status(500).send('Internal Server Error');
     }
-}
+};
 
-export default new CategoryController();
+export default {
+    getAllCategories,
+    createCategory,
+    updateCategory,
+    deleteCategory
+};
